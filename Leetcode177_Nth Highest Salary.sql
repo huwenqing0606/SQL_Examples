22ef037ad5dcb12f6e7ae6a400d4cabf655eb1e9
Dr_HuWenqing <huwenqing.pku@gmail.com>
2021-06-01 23:13:09 -0500

    Update Leetcode177_Nth Highest Salary.sql

--- Leetcode177_Nth Highest Salary.sql
+++ Leetcode177_Nth Highest Salary.sql
@@ -1,3 +1,10 @@
-use [TutorialDB]
+create function getNthHighestSalary(@N int) returns int
+begin
+	declare @p int;
+	set @p = 1;
+	select top 5 Evaluation as eva, Name as nme from dbo.Customers order by Evaluation; 
+	return 0;
+end;
 
-select distinct top 2 * from dbo.Customers order by Evaluation desc;
+use [TutorialDB];
+getNthHighestSalary(1);
